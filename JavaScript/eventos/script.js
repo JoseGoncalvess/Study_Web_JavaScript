bc0f625c3cb64f4592fc930f2e8c119278c9351e
JoseGoncalvess <joseinacio774@gmail.com>
2024-06-28 16:11:00 -0300

    Event by Js

---
+++ JavaScript/eventos/script.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+    console.log("O mouse acabou de entrar");
+}
+
+function onChange() {
+    console.log("Mudou Valor")
+}
+
+function OnFocus() {
+    console.log("Focou do Elemento")
+}
+
+function onBlur() {
+    console.log("Desfou do Elemento")
+}
+
+function onClick() {
+    console.log("Acabou de clicar no elemento")
+}
