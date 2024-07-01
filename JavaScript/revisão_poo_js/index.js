9dd484cb6958e7542080aaa79cac86c7c1c76117
JoseGoncalvess <joseinacio774@gmail.com>
2024-07-01 09:03:34 -0300

    Poo in JavaScript

--- JavaScript/revisão_poo_js/index.js
+++ JavaScript/revisão_poo_js/index.js
@@ -1,17 +1,18 @@
 class Book {
-  titulo;
-  autor;
-  page;
-  category;
-
-  book(titulo, autor, page, category) {
+  constructor(titulo, autor, page, category) {
     this.titulo = titulo;
     this.autor = autor;
     this.page = page;
     this.category = category;
   }
+
+  thisisBook(){
+    console.log(`O lirvro ${this.titulo} possui ${this.page}`)
+  }
 }
 
-const book = new Book().book("Do mil ao Milhão ser cortar o caferzinho","Primo Rico",180.,["Investimento", "Dicdatico"])
+const book = new Book("Do mil ao Milhão ser cortar o caferzinho","Primo Rico",180.,["Investimento", "Dicdatico"])
 
 console.log(book.titulo)
+console.log(book.category)
+console.log(book.page)
