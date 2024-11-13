4ae58f860f5f02f25ca0fe5c2f217898d1e69421
JoseGoncalvess <joseinacio774@gmail.com>
2024-11-13 17:53:30 -0300

    Operador Null

---
+++ JavaScript/Conteúdo/Operador de Coalescência Nula/index.js
@@ -0,0 +1,13 @@
+
+// SE FOR NULL ELE RETORNA OUTRO VALOR 
+let nome = null
+
+console.log(nome?? 0)
+
+let a = 0
+let b = null
+let c = "DEU CERTO"
+
+console.log(a || b || c)
+
+// OS DOIS OPERDORES SÂO OTIMOS PARA LIDAR COM POSSIBILIDADE D EVALORES NULL  OU UNDEFINED
