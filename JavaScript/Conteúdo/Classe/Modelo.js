1201f93b44c4bad454ff40589b4280220a6452ca
José Gonçalves <joseinacio774@gmail.com>
2025-04-23 23:18:32 -0300

    Learn Objetct

--- JavaScript/Conteúdo/Classe/Modelo.js
+++ JavaScript/Conteúdo/Classe/Modelo.js
@@ -9,6 +9,20 @@ class SmartPhone {
   }
 }
 
-let myPhone = new SmartPhone("Redmi Note 5",2017)
+let myPhone = new SmartPhone("Redmi Note 5", 2017)
 
 myPhone.ViewPhone()
+
+//  UMA CLASSE E SEU METODO COSNSTRUTOR NAD AMAIS È QUE UMA FUNCTION QUE RETONA UM PROPOTIPO
+
+let FsmartPhone = {
+  model: "2024",
+  year: "2020"
+}
+
+const sansung = {
+  model: "S 23",
+  __proto__: FsmartPhone
+}
+
+console.log(sansung.year);
